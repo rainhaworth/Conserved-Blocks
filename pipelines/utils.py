0446a2b180996bbc15c5a1678acd4f868b9e54b0
rainhaworth <rhaworth@umd.edu>
2023-03-02 00:20:24 -0500

    many changes; pipeline should be nearly functional

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -1,22 +1,289 @@
 # Reusable helper functions for impementing pipelines
 
+import os
 import numpy as np
 import tensorflow as tf
 from dna2vec.dna2vec.multi_k_model import MultiKModel
+from gensim.models import word2vec
 
-# TODO: make k-mer embedding into a tf.keras.layers.Layer
+from pipelines.d2v_bigbird_base import TransformerClusterModel
 
-# Generate a MultiKModel
-def init_dna2vec():
-    filepath = 'pretrained/dna2vec-20161219-0153-k3to8-100d-10c-29320Mbp-sliding-Xat.w2v'
-    return MultiKModel(filepath)
+from bigbird.core import modeling
+from bigbird.core import utils
+from bigbird.core import optimization
 
-# K-mer embedding from config string
-def contig_to_kmers(contigstr, mk_model):
-    length = len(contigstr) - 8 + 1
-    embed = tf.zeros([length, 100])
-    for i in range(length):
-        embed[i] = mk_model.vector(contigstr[i:i+8])
-    return embed
+# TODO: implement reverse complement w/ lexicographical comparison
+
+
+# Define input_fn_builder, modified from run_summarization.py
+# If this has issues, just double return everything
+def input_fn_builder(data_dir, vocab_model_file, max_encoder_length,
+                     max_decoder_length, is_training):
+    """Creates an `input_fn` closure to be passed to TPUEstimator."""
+
+    def _decode_record(record):
+        """Decodes a record to a TensorFlow example."""
+        name_to_features = {
+            "contig": tf.io.FixedLenFeature([], tf.string)
+        }
+        example = tf.io.parse_single_example(record, name_to_features)
+        return example["contig"]
+
+    def _tokenize_contig(contig):
+        # Initialize tokenizer
+        # TODO: test whether tft.SentencepieceTokenizer can work with dna2vec somehow
+            # I don't think so
+        # Also is this being called repeatedly? I really hope not or I have to fix that
+        tokenizer = D2v8merTokenizer()
+
+        # For this implementation, we use the same contig for input and output
+        contig_ids = tokenizer.tokenize(contig)
+        if isinstance(contig_ids, tf.RaggedTensor):
+            contig_ids = contig_ids.to_tensor(0)
+        contig_ids = contig_ids[:max_encoder_length]
+
+        return contig_ids
+
+    def input_fn(params):
+        """The actual input function."""
+        batch_size = params["batch_size"]
+
+        # Load dataset from text files
+        input_files = tf.io.gfile.glob(
+            os.path.join(data_dir, "*.txt"))
+
+        # For training, we want a lot of parallel reading and shuffling.
+        # For eval, we want no shuffling and parallel reading doesn't matter.
+        if is_training:
+            d = tf.data.Dataset.from_tensor_slices(tf.constant(input_files))
+            d = d.shuffle(buffer_size=len(input_files))
+
+            # Non deterministic mode means that the interleaving is not exact.
+            # This adds even more randomness to the training pipeline.
+            d = d.interleave(tf.data.TFRecordDataset,
+                                deterministic=False,
+                                num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        else:
+            d = tf.data.TFRecordDataset(input_files)
+
+        d = d.map(_decode_record,
+                num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                deterministic=is_training)
+
+        d = d.map(_tokenize_contig,
+                    num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                    deterministic=is_training)
+
+        if is_training:
+            d = d.shuffle(buffer_size=10000, reshuffle_each_iteration=True)
+            d = d.repeat()
+        d = d.padded_batch(batch_size, ([max_encoder_length], [max_decoder_length]),
+                            drop_remainder=True)  # For static shape
+        return d
+
+    return input_fn
+
+# Define model_fn_builder, modified from run_summarization.py
+def model_fn_builder(transformer_config):
+  """Returns `model_fn` closure for TPUEstimator."""
+
+  def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
+    """The `model_fn` for TPUEstimator."""
+
+    # idk if we need this
+    if isinstance(features, dict):
+      if not labels and "target_ids" in features:
+        labels = features["target_ids"]
+      features = features["input_ids"]
+
+    is_training = (mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Changed model
+    model = TransformerClusterModel(transformer_config)
+    (llh, logits, pred_ids), _ = model(features, target_ids=labels,
+                                       training=is_training)
+
+    # Use custom loss function
+    total_loss = loss_fn(logits, labels)
+
+    tvars = tf.compat.v1.trainable_variables()
+    utils.log_variables(tvars, transformer_config["ckpt_var_list"])
+
+    output_spec = None
+    if mode == tf.estimator.ModeKeys.TRAIN:
+
+        learning_rate = optimization.get_linear_warmup_rsqrt_decay_lr(
+            init_lr=transformer_config["learning_rate"],
+            hidden_size=transformer_config["hidden_size"],
+            num_warmup_steps=transformer_config["num_warmup_steps"])
+
+        optimizer = optimization.get_optimizer(transformer_config, learning_rate)
+
+        global_step = tf.compat.v1.train.get_global_step()
+
+        # TODO: decide if we're using position embedding
+        #if not transformer_config["use_bias"]:
+        #    logging.info("Fixing position embedding, i.e. not trainable.")
+        #    posemb = "pegasus/embeddings/position_embeddings"
+        #    tvars = list(filter(lambda v: v.name.split(":")[0] != posemb, tvars))
+
+        gradients = optimizer.compute_gradients(total_loss, tvars)
+        train_op = optimizer.apply_gradients(gradients, global_step=global_step)
+
+        output_spec = tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
+            mode=mode,
+            loss=total_loss,
+            train_op=train_op,
+            host_call=utils.add_scalars_to_summary(
+                transformer_config["output_dir"],
+                {"learning_rate": learning_rate}))
+
+    elif mode == tf.estimator.ModeKeys.EVAL:
+        tokenizer = D2v8merTokenizer()
+
+        # TODO: define some kind of metric for evaluation, e.g. edit distance
+        # Or I guess it would have to be alignment or something?
+        # You know what maybe this whole part is too hard without external tools
+        def eval_func(label_sent, pred_sent):
+            return label_sent - pred_sent
+
+        def metric_fn(loss, log_probs, label_ids, pred_ids):
+            loss = tf.compat.v1.metrics.mean(values=loss)
+            log_probs = tf.compat.v1.metrics.mean(
+                values=log_probs,
+                weights=tf.cast(tf.not_equal(label_ids, 0), tf.float32))
+            metric_dict = {
+                "prediction_loss": loss,
+                "log_likelihood": log_probs,
+            }
+
+            if not transformer_config["use_tpu"]:
+                # Approximate ROUGE scores if not running on tpus.
+                # Always run externally for final scores.
+                label_sent = tokenizer.detokenize(label_ids)
+                pred_sent = tokenizer.detokenize(pred_ids)
+                
+                # TODO: make this use whatever eval score instead of ROUGE score
+                rouge_value = tf.compat.v1.py_func(
+                    func=eval_func,
+                    inp=[label_sent, pred_sent],
+                    Tout=[tf.float64, tf.float64, tf.float64],
+                    stateful=False)
+                rouge_value = tf.cast(rouge_value, tf.float32)
+                rouge1 = tf.compat.v1.metrics.mean(values=rouge_value[0])
+                rouge2 = tf.compat.v1.metrics.mean(values=rouge_value[1])
+                rougeL = tf.compat.v1.metrics.mean(values=rouge_value[2])  # pylint: disable=invalid-name
+
+                metric_dict.update({
+                    "eval/Rouge-1": rouge1,
+                    "eval/Rouge-2": rouge2,
+                    "eval/Rouge-L": rougeL,
+                })
+            return metric_dict
+
+        eval_metrics = (metric_fn,
+                        [total_loss, llh, labels, pred_ids])
+        output_spec = tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
+            mode=mode,
+            loss=total_loss,
+            eval_metrics=eval_metrics)
+    else:
+        prediction_dict = {"pred_ids": pred_ids}
+        if not transformer_config["use_tpu"]:
+            tokenizer = D2v8merTokenizer()
+
+            pred_sent = tokenizer.detokenize(pred_ids)
+
+            prediction_dict.update({"pred_sent": pred_sent})
+
+        output_spec = tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
+            mode=mode,
+            predictions=prediction_dict)
+
+    return output_spec
+
+  return model_fn
+
+# Define loss function
+def loss_fn(logits, labels):
+    return tf.nn.softmax_cross_entropy_with_logits(logits, labels)
+
+# Define dna2vec tokenize/detokenize wrapper
+class D2v8merTokenizer():
+    def __init__(self):
+        # This makes it only work from the base directory (Conserved-Blocks)
+        # Maybe make the directory a parameter?
+        filepath = 'dna2vec/pretrained/dna2vec-8mer-only.w2v'
+        self.model = word2vec.KeyedVectors.load_word2vec_format(filepath, binary=False)
+    def tokenize(self, seq):
+        length = len(seq) - 8 + 1
+        embed = tf.zeros([length, 100])
+        for i in range(length): # - 8 + 1
+            embed[i] = self.model.vector(seq[i:i+8])
+        return np.array(embed)
+
+    def detokenize(self, embed):
+        # The difficult part here is that there may be conflicts
+        # TODO: handle those somehow
+        # For now, just fetch 8-mers
+        seq = ''
+        for i in range(0, embed.shape(0), 8):
+            tmp = self.model.similar_by_vector(embed[i], topn=1)
+            seq += tmp[0]
+        # NOTE: this also drops the last few nucleotides if len % 8 != 0, but w/e it's not a good solution anyway
+        return seq
+
+# Define dna2vec embedding layer class
+# Base implementation with no positional information beyond 8-mers
+# TODO: figure out if we should do this instead lol
+class D2vKmerEmbeddingLayer(tf.keras.layers.Layer):
+    """An embedding layer."""
+
+    def __init__(self,
+                initializer,
+                scale_emb=False,
+                name="embeddings"):
+        super(D2vKmerEmbeddingLayer, self).__init__(name=name)
+        # Vocab size should always be 65,536, embedding dim should always be 100
+        self.vocab_size = 65536
+        self.emb_dim = 100
+        self.scale_emb = scale_emb
+
+        with tf.compat.v1.variable_scope(name):
+            self.word_embeddings = tf.compat.v1.get_variable(
+                "word_embeddings", [self.vocab_size, self.emb_dim],
+                dtype=tf.float32, initializer=initializer)
+
+    def call(self,
+            input_ids,
+            seq_length,
+            start_pos=0,
+            training=None):
+        if input_ids is None:
+            return None
+
+        # subtoken embedding
+        output = tf.nn.embedding_lookup(params=self.word_embeddings, ids=input_ids)
+
+        if self.scale_emb:
+            output = output * self.emb_dim ** 0.5
+        
+        return output
+
+    def linear(self, x):
+        """Computes logits by running x through a linear layer.
+
+        Args:
+        x: A float32 tensor with shape [..., hidden_size]
+        Returns:
+        float32 tensor with shape [..., vocab_size].
+        """
+        with tf.compat.v1.name_scope("presoftmax_linear"):
+            logits = tf.tensordot(x, self.word_embeddings, [[-1], [1]])
+        return logits
 
 # TODO: vector-based clustering
+# god why is this so fucking hard
+# okay i guess i'll just try to implement like, whichever one is the easiest. probably k-means.
+# then i need a function to find the centroid.
+# maybe test everything else first?
--- train.py
+++ train.py
@@ -1,1 +1,133 @@
 # Train model
+
+import pipelines.utils
+from pipelines.d2v_bigbird_base import TransformerClusterModel
+
+#from bigbird.core import utils
+#from bigbird.core import modeling
+from bigbird.core import flags
+
+import tensorflow.compat.v2 as tf
+from tqdm import tqdm
+import sys
+
+# From ipynb: create container
+# not sure what it does but it somehow, um, contains the model
+from tensorflow.python.ops.variable_scope import EagerVariableStore
+container = EagerVariableStore()
+
+#"""
+
+# Set flags, slightly modified from pumbed.ipynb
+FLAGS = flags.FLAGS
+if not hasattr(FLAGS, "f"): flags.DEFINE_string("f", "", "")
+FLAGS(sys.argv)
+
+tf.enable_v2_behavior()
+
+# it keeps yelling at me about the flags
+# do i have to just define them all?
+# i think i should put this on the cluster
+
+# data_dir should probably be a parameter
+FLAGS.data_dir = "./data-tmp/"
+FLAGS.attention_type = "block_sparse"
+FLAGS.couple_encoder_decoder = True
+FLAGS.max_encoder_length = 4096
+FLAGS.max_decoder_length = 4096
+FLAGS.block_size = 64
+FLAGS.learning_rate = 1e-5
+FLAGS.num_train_steps = 10000
+FLAGS.attention_probs_dropout_prob = 0.0
+FLAGS.hidden_dropout_prob = 0.0
+FLAGS.use_gradient_checkpointing = True
+# idk if i need vocab_model_file at all? leaving it out for now
+#FLAGS.vocab_model_file = "gpt2"
+
+print(flags.as_dictionary())
+
+# Init params, model, config
+# I used to have a utils.BigBirdConfig() here but I prefer the flags, dropping that if possible
+
+config = flags.as_dictionary()
+
+with container.as_default():
+    model = TransformerClusterModel(config)
+
+# Call input and model function builders
+train_input_fn = pipelines.utils.input_fn_builder(
+    data_dir=FLAGS.data_dir,
+    vocab_model_file=FLAGS.vocab_model_file,
+    max_encoder_length=FLAGS.max_encoder_length,
+    max_decoder_length=FLAGS.max_decoder_length,
+    is_training=True)
+# Idk if this is the right batch size to use but it's in the notebook
+dataset = train_input_fn({'batch_size': 8})
+
+model_fn = pipelines.utils.model_fn_builder(flags)
+
+# Define forward + backward pass, idk if this is what we need to do lol but it's from the file
+# I think this is just like, a training function with backpropogation (the gradient tape thing)
+# And the eval function is just the model part
+# Should this go in the utils file? possibly
+@tf.function(experimental_compile=True)
+def fwd_bwd(features, labels):
+  with tf.GradientTape() as g:
+    (llh, logits, pred_ids), _ = model(features, target_ids=labels,
+                                       training=True)
+    loss = pipelines.utils.loss_fn(
+        logits, labels) # Include transformer config if needed
+  grads = g.gradient(loss, model.trainable_weights)
+  return loss, llh, logits, pred_ids, grads
+
+# inspect at a few examples
+#for ex in dataset.take(3):
+#  print(ex)
+
+# check outputs
+#loss, llh, logits, pred_ids, grads = fwd_bwd(ex[0], ex[1])
+#print('Loss: ', loss)
+
+# see ipynb if loading pretrained
+
+# Train model
+opt = tf.keras.optimizers.Adam(FLAGS.learning_rate)
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+# NOTE: the ex[0] and ex[1] should break; if the point of failure is here, change both to ex or ex[0]
+  # but if the point of failure is somewhere in input_fn_builder, we can change that instead
+for i, ex in enumerate(tqdm(dataset.take(FLAGS.num_train_steps), position=0)):
+  loss, llh, logits, pred_ids, grads = fwd_bwd(ex[0], ex[1])
+  opt.apply_gradients(zip(grads, model.trainable_weights))
+  train_loss(loss)
+  if i% 10 == 0:
+    print('Loss = {} '.format(train_loss.result().numpy()))
+
+print("Training done. Evaluating...")
+
+# Eval code
+# TODO: make new file or add a conditional or smth
+@tf.function(experimental_compile=True)
+def fwd_only(features, labels):
+  (llh, logits, pred_ids), _ = model(features, target_ids=labels,
+                                       training=False)
+  return llh, logits, pred_ids
+
+eval_input_fn = pipelines.utils.input_fn_builder(
+        data_dir=FLAGS.data_dir,
+        vocab_model_file=FLAGS.vocab_model_file,
+        max_encoder_length=FLAGS.max_encoder_length,
+        max_decoder_length=FLAGS.max_decoder_length,
+        is_training=False)
+eval_dataset = eval_input_fn({'batch_size': 8})
+
+eval_llh = tf.keras.metrics.Mean(name='eval_llh')
+
+for ex in tqdm(eval_dataset, position=0):
+  llh, logits, pred_ids = fwd_only(ex[0], ex[1])
+  eval_llh(llh)
+print('Log Likelihood = {}'.format(eval_llh.result().numpy()))
+
+# to get predictions, call:
+#_, _, pred_ids = fwd_only(ex[0], ex[1])
+# then detokenize pred_ids
